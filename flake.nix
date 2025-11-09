9a40103cba333886de1e16570c77472bcdb3a05e
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-11-09 19:04:53 +0100

    bump

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
           (final: prev: {
             docker = prev.docker.overrideAttrs (old: {
               postFixup = (old.postFixup or "") + ''
-                touch $out/5
+                touch $out/6
               '';
             });
             failing-package = prev.stdenv.mkDerivation {
