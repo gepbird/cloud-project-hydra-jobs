60beeabd801b162c82162394ff6ba8e596bcc66c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-11-09 21:00:50 +0100

    add patch to docker

--- flake.nix
+++ flake.nix
@@ -11,8 +11,18 @@
         overlays = [
           (final: prev: {
             docker = prev.docker.overrideAttrs (old: {
-              postFixup = (old.postFixup or "") + ''
-                touch $out/6
+              patches = [
+                (prev.fetchpatch {
+                  # https://github.com/docker/cli/pull/6048
+                  name = "enhance-docker-system-prune-performance-via-concurrent-pruning.patch";
+                  url = "https://github.com/docker/cli/pull/6048/commits/255b625672a85506d4a5782c4e250175d307ee45.patch";
+                  hash = "sha256-Az9P/6SmSBtykE4gQNTG0H8MHqqb4CpQyYg468q42KA=";
+                })
+              ];
+              preBuild = ''
+                substituteInPlace "cli/cobra.go" --replace-fail \
+                  "For more help on how to use Docker, head to https://docs.docker.com/go/guides/" \
+                  "For more help on how to use Docker, head to https://docs.docker.com/go/guides/\nThis version of docker is modified and was build on Hydra."
               '';
             });
             failing-package = prev.stdenv.mkDerivation {
